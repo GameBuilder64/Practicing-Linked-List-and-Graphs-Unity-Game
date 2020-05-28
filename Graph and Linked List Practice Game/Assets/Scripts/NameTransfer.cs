8cb676d1768dc1354121a6690b111be13a0d30f6
GameBuilder64 <64383913+GameBuilder64@users.noreply.github.com>
2020-05-28 01:03:14 -0700

    Just Added input Text

---
+++ Graph and Linked List Practice Game/Assets/Scripts/NameTransfer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Unity;
+
+public class NameTransfer : MonoBehaviour
+{
+    public string theName;
+    public GameObject inputField;
+    public GameObject textDisplay;
+   
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StoreName()
+    {
+        theName = inputField.GetComponent<Text>().text;
+        textDisplay.GetComponent<Text>().text = "Welcome " + theName + " to the game";
+    }
+
+
+}
